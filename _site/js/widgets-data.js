e155c08ee55b1257449f60cd011e9ba3dbbcb6b8
nawabahmadreshi <reshi.nawab@gmail.com>
2018-11-18 19:08:28 +0530

    update

---
+++ _site/js/widgets-data.js
@@ -0,0 +1,20 @@
+var widgetsData = {  
+   versions:[  
+      {  
+         "title":"1.9",
+         "branch":"1.9",
+         "desc":"",
+         "releaseDate":"22nd Aug",
+         "latest":true,
+         "published":true
+      },
+      {  
+         "title":"1.8",
+         "branch":"1.8",
+         "desc":"",
+         "releaseDate":"6th July",
+         "latest":false,
+         "published":true
+      }
+   ]
+}
