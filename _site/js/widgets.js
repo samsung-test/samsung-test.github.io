50a7a58982dae67d88a19fd2560d8b97c6c7b98f
nawabahmadreshi <reshi.nawab@gmail.com>
2019-09-27 15:27:22 +0530

    update

---
+++ _site/js/widgets.js
@@ -0,0 +1,16 @@
+publishedVersions = widgetsData.versions.filter(function(version){
+	return version.published === true;
+});
+latestVersion = widgetsData.versions.filter(function(version){
+	return version.published === true && version.latest === true ;
+});
+currentVersion = $('#docsVersion').attr("content");
+currentVersionData = widgetsData.versions.find( version => version.title === currentVersion);
+
+$(function(){
+	$("#templates").load("/js/templates.html"); 
+});
+console.log('published version',publishedVersions);
+console.log('latest version',latestVersion);
+console.log('current version',currentVersionData);
+//console.log('current url',currentUrl);
