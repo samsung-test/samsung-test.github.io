28eddb2c71a3609de6b485a2ee38eababeef38d9
nawabahmadreshi <reshi.nawab@gmail.com>
2019-09-28 11:04:21 +0530

    update

---
+++ js/widgets-data.js
@@ -0,0 +1,20 @@
+var widgetsData = {  
+   versions:[ 
+	  {  
+         "title":"draft",
+         "branch":"draft",
+         "desc":"",
+         "releaseDate":"22nd Aug",
+         "latest":true,
+         "published":true
+      },
+      {  
+         "title":"1.0",
+         "branch":"1.0",
+         "desc":"",
+         "releaseDate":"21st Nov",
+         "latest":false,
+         "published":true
+      }
+   ]
+}
