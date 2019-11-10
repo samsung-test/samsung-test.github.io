51e6871efef2f7e68450f073cc66237d5c11f03e
The Anonymous Person <55868935+theanonymousperson@users.noreply.github.com>
2019-11-10 18:06:55 +0530

    update

--- js/widgets.js
+++ js/widgets.js
@@ -15,3 +15,27 @@ console.log('published version',publishedVersions);
 console.log('latest version',latestVersion);
 console.log('current version',currentVersion);
 console.log('current version Data',currentVersionData);
+
+
+/*
+ *
+ * toggle menu *********************************************************************
+ *
+ */
+
+$("#menu-toggle").click(function(e) {
+	console.log('menu clicked');
+	e.preventDefault();
+	$(".wrapper").toggleClass("right-open");
+	$(".col-toc").toggleClass("col-toc-hidden");   
+  });
+  $("#menu-toggle-left").click(function(e) {
+	e.preventDefault();
+	$(".col-nav").toggleClass("col-toc-hidden");
+  });
+  $(".navbar-toggle").click(function(){
+	$("#sidebar-nav").each(function(){
+	  $(this).toggleClass("hidden-sm");
+	  $(this).toggleClass("hidden-xs");
+	});
+  });
