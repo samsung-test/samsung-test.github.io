d75fd8d63f7a852a28fa0471a39d5802b39c0a0a
The Anonymous Person <55868935+theanonymousperson@users.noreply.github.com>
2019-11-17 22:42:33 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -93,7 +93,7 @@ layout: null
 		searchInput = document.getElementById("st-search-input");
 
 	searchQueryEl.innerText = query;
-	searchInput.innerText = query;
+	searchInput.value = query;
 	searchQueryContainerEl.style.display = "inline";
 
 	for (var key in window.data) {
