7efcf0058381b0fb02ca547a2c52d86291a6e27a
The Anonymous Person <55868935+theanonymousperson@users.noreply.github.com>
2019-12-22 21:17:22 +0530

    Update widgets-data.js

--- js/widgets-data.js
+++ js/widgets-data.js
@@ -1,7 +1,7 @@
 var widgetsData = {  
    versions:[ 
       {  
-         "title":"Tizen 5.5",
+         "title":"5.5",
          "branch":"5.5",
          "desc":"",
          "releaseDate":"21st Nov",
@@ -9,7 +9,7 @@ var widgetsData = {
          "published":true
       },
       {  
-         "title":"Tizen 5.0",
+         "title":"5.0",
          "branch":"5.0",
          "desc":"",
          "releaseDate":"21st Nov",
